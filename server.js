780e5286a9bfce78aaa5c6bd3d5c17e9f120ee92
Andrii Ivanytskyi <andriiivanytskyi@gmail.com>
2020-06-15 13:17:13 -0700

    api key

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');
+sgMail.setApiKey('###');
 
 app.use(express.json()) 
 app.use (express.urlencoded({extended: false}))
